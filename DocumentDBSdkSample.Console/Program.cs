ff1a3dd1ebe2da87ebcdaeefba8d51874c54222f
tsubakimoto <tkmbmbr@gmail.com>
2016-12-03 16:57:25 +0900

    define parameters

--- DocumentDBSdkSample.Console/Program.cs
+++ DocumentDBSdkSample.Console/Program.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
 
 namespace DocumentDBSdkSample.Console
 {
     public class Program
     {
+        private const string EndpointUri = "<your endpoint URI>";
+        private const string PrimaryKey = "<your key>";
+        private DocumentClient client;
+
         public static void Main(string[] args)
         {
         }
